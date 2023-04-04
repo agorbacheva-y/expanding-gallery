38c237ea3bf794656abfc972b52b141d6cc30b45
agorbacheva-y <agorbacheva.y@gmail.com>
2023-04-04 09:11:10 +0200

    codealong

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const panels = document.querySelectorAll(".panel");
 
+//console.log(panels[0]);
+
+// loop over panels and add event listener (click) to expand image
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+
+        removeActiveClass();
+
+        // add active class when click
+        panel.classList.add("active")
+    });
+});
+
+function removeActiveClass() {
+    panels.forEach((panel) => {
+        // remove active class
+        panel.classList.remove("active")
+    });
+}
